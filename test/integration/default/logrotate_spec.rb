e51aee2e7e24fbb1402c61195c227ee3d9d13632
Chris Sullivan <email-blocked>
2017-03-18 21:12:30 +0000

    Added some tests around Chef client service and log file

--- test/integration/default/logrotate_spec.rb
+++ test/integration/default/logrotate_spec.rb
@@ -18,3 +18,15 @@ end
 describe file('c:\chef\log\client.log.20170201') do
   it { should exist }
 end
+
+# Chef client running as a service
+describe service('chef-client') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# Chef service using different log from Chef-client
+describe file('c:\chef\log\client.service.log') do
+  it { should exist }
+end
